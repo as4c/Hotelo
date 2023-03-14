d57ebbef5d84c0d75d8db908cb7a97716f59f5ee
sagar kumar <sagarkumarsameer@gmail.com>
2023-03-14 12:33:46 +0530

    txt1

--- hotel/settings.py
+++ hotel/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'HotelDb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'hotel_db',
+    #     'USER': 'railway',
+    #     'PASSWORD': '',
+    #     'HOST': 'mysql',
+    #     'PORT': '3306',
+    # }
 }
 
 
